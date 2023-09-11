ec28114ba1ae892176befe675dd6120703c6edbc
Teodora1998 <p.teodora31@yahoo.com>
2023-09-11 22:21:29 +0300

    complet

--- src/index.js
+++ src/index.js
@@ -2,9 +2,15 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import { App } from 'components/App';
 import './index.css';
+import 'bootstrap/dist/css/bootstrap.min.css'; 
+import 'bootstrap/dist/js/bootstrap.bundle.min.js'; 
+import { BrowserRouter } from "react-router-dom";
 
-ReactDOM.createRoot(document.getElementById('root')).render(
+ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <App />
+    <BrowserRouter basename="/goit-react-hw-05-movies">
+      <App />
+    </BrowserRouter>
   </React.StrictMode>
 );
+ 
